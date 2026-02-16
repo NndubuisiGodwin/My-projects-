a6db4c7937a80be9d3a21717abc0b45623ddbc53
NndubuisiGodwin <reazonwell2010@gmail.com>
2026-02-16 18:46:53 +0100

    Add files via upload
    A simple login system that asks for a username and password. I made it to understand how authentication works and to practice using variables and conditions.

---
+++ login_system.py
@@ -0,0 +1,8 @@
+username = "admin"
+password = "1234"
+u = input("Username: ")
+p = input("Password: ")
+if u == username and p == password:
+    print("Login successful")
+else:
+    print("Login failed")
